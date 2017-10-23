ec742c16d1f207370b35b35788e2c17b7057f9e0
barlaensdoonn <brandon.aleson@gmail.com>
2017-10-22 19:34:09 -0700

    add utility sketch to test relay

---
+++ arduino/test_relay/test_relay.ino
@@ -0,0 +1,28 @@
+// use light switch as trigger for relay
+// 10/22/17
+
+// using Power Switch Tail (PST) as relay http://www.powerswitchtail.com/
+// pin 7 connected to PST + input
+// ground connected to PST - input
+
+int relay = 7;  // pin used to communicate with relay
+int delayTime = 3000;  // time to wait between switches
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, LOW);
+  Serial.println("Setup has ended, entering loop()");
+}
+
+void loop() {
+  digitalWrite(relay, HIGH);
+  Serial.println("wrote relay pin HIGH");
+  delay(delayTime);
+
+  digitalWrite(relay, LOW);
+  Serial.println("wrote relay pin LOW");
+  delay(delayTime);
+}
+
